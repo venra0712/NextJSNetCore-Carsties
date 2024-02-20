a87a18f10f381a3d1b69cdcbd6964ba333e6c742
Ven Ra <73223734+venra0712@users.noreply.github.com>
2024-02-20 13:44:56 +0800

    End of Section 3

--- src/SearchService/Models/Item.cs
+++ src/SearchService/Models/Item.cs
@@ -1,8 +1,9 @@
+using MongoDB.Entities;
+
 namespace SearchService.Models
 {
-    public class Item
+    public class Item : Entity
     {
-        public Guid Id { get; set; }   
         public int ReservePrice { get; set; }
         public string Seller { get; set; }
         public string Winner { get; set; }
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,8 +1,20 @@
+using System.Net;
+using System.Runtime.ConstrainedExecution;
+using MongoDB.Driver;
+using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
+using SearchService.Data;
+using SearchService.Models;
+using SearchService.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddHttpClient<AuctionServiceHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
@@ -10,4 +22,22 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.Lifetime.ApplicationStarted.Register(async () => 
+{
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+});
+
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy() 
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
